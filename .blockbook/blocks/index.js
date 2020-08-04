5236c94f4812cd893905feee1c2ba6954d305e7d
Fabian Kägy <mail@fabian-kaegy.de>
2020-08-04 08:19:31 +0200

    add core blocks

---
+++ .blockbook/blocks/index.js
@@ -0,0 +1,36 @@
+import {
+	registerBlockType,
+} from 'blockbook-api';
+
+// Register BlockBook blocks.
+const coreBlocks = [
+	'core/audio',
+	'core/buttons',
+	'core/code',
+	'core/columns',
+	'core/cover',
+	'core/embed',
+	'core/file',
+	'core/gallery',
+	'core/group',
+	'core/heading',
+	'core/html',
+	'core/image',
+	'core/latest-posts',
+	'core/latestComments',
+	'core/list',
+	'core/media-text',
+	'core/paragraph',
+	'core/preformatted',
+	'core/pullquote',
+	'core/quote',
+	'core/search',
+	'core/separator',
+	'core/shortcode',
+	'core/social-links',
+	'core/spacer',
+	'core/table',
+	'core/verse',
+	'core/video',
+];
+coreBlocks.forEach( ( block ) => registerBlockType( block ) );
